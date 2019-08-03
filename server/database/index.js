13ea5aab6618f46f6742cbd61d71cf4601e55433
Tom Cernera <tomcernera@gmail.com>
2019-08-02 22:52:59 -0400

    Meta query using promise all, needs formatting

--- server/database/index.js
+++ server/database/index.js
@@ -1,68 +1,58 @@
 const { Pool } = require("pg");
 
-// const pool = new Pool({
-//   user : 'postgres',
-//   host : 'postgres',
-//   database: "sdc",
-//   password: 'supersecret',
-//   port: 5321
-// });
-
-const connectionString = 'postgresql://localhost:5432/sdc'
+const connectionString = "postgres://localhost:5432/sdc";
 const pool = new Pool({
-  connectionString: connectionString,
-})
+  connectionString: connectionString
+});
+
 module.exports = {
   listAll: (req, res) => {
-    pool.query(
-      `SELECT list_reviews.*, array_agg(review_photos.url) AS photos FROM list_reviews
+    console.time()
+    pool
+      .query(
+        `SELECT list_reviews.*, array_agg(review_photos.url) AS photos FROM list_reviews
        LEFT JOIN review_photos
        ON review_photos.review_id = list_reviews.id
        WHERE list_reviews.product_id = ${req.params.product_id} AND list_reviews.reported = false
-       GROUP BY list_reviews.id;`,
-      (err, results) => {
-        if (err) {
-          console.log(err);
-        }
-        res.send(results.rows);
-        console.log(results.rows)
-        // pool.end() when to use pool.end()?
-      }
-  )},
-  meta : (req, res) => {
-    pool.query(
-      `SELECT rating, COUNT(rating) FROM list_reviews 
+       GROUP BY list_reviews.id;`)
+      .then(results => {res.send(results.rows);console.timeEnd()})
+      .catch(err => console.log(err));
+  },
+  meta: (req, res) => {
+    console.time();
+    Promise.all([
+    pool
+      .query(
+        `SELECT rating, COUNT(rating) FROM list_reviews 
        WHERE product_id = ${req.params.product_id} 
-       GROUP BY rating;`,
-      (err, results) => {
-        if (err) {
-          console.log(err)
-        }
-        res.send(results.rows);
-      }
-    )},
-  helpful : (req,res) => {
+       GROUP BY rating;`
+      ),
     pool.query(
-      `UPDATE list_reviews
+      `SELECT recommend, COUNT(recommend) FROM list_reviews WHERE product_id = ${req.params.product_id} AND recommend = true GROUP BY recommend`),
+    pool.query(
+      `Select characteristic_id, AVG(value) FROM characteristic_reviews 
+       WHERE characteristic_id IN (SELECT id from characteristics where product_id = ${req.params.product_id}) 
+       GROUP BY characteristic_id;`
+    )
+    ]).then(results => {results.map(result => console.log(result.rows));console.timeEnd();res.sendStatus(200)})
+  },
+  helpful: (req, res) => {
+    pool
+      .query(
+        `UPDATE list_reviews
        SET helpfulness = helpfulness + 1
-       WHERE id = ${req.params.review_id}`,
-       (err, results) => {
-         if (err) {
-           console.log(err)
-         }
-         res.send('marked as helpful');
-        }
-    )},
-  reported : (req,res) => {
-    pool.query(
-    `UPDATE list_reviews
+       WHERE id = ${req.params.review_id}`
+      )
+      .then(() => res.sendStatus(202))
+      .catch(err => console.log(err));
+  },
+  reported: (req, res) => {
+    pool
+    .query(
+      `UPDATE list_reviews
        SET reported = NOT reported
-       WHERE id = ${req.params.review_id}`,
-    (err, results) => {
-      if (err) {
-        console.log(err)
-      }
-      res.send('reported')
-    })
+       WHERE id = ${req.params.review_id}`)
+    .then(()=> res.sendStatu(202))
+    .catch(err => console.log(err))
   }
 };
