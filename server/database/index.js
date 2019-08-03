1620debff6b8b66c342bc697982a83cd5b02d1ed
Tom Cernera <tomcernera@gmail.com>
2019-08-03 13:13:41 -0400

    Meta formatting done

--- server/database/index.js
+++ server/database/index.js
@@ -13,7 +13,7 @@ const listallFormat = (q) => {
       review_id : q[i].id,
       rating : q[i].rating,
       summary : q[i].summary,
-      recommend : q[i].recommend,
+      recommend : q[i].recommend === true ? 1 : 0,
       response : q[i].response,
       body : q[i].body,
       date : q[i].date,
@@ -31,6 +31,40 @@ const listallFormat = (q) => {
   return listAllObj;
 };
 
+const metaFormat = (q,id) => {
+  console.log(q[2].rows);
+  let ratings = {};
+  q[0].rows.forEach(rating => {
+    ratings[rating.rating] = rating.count
+  })
+
+  let recommended ={0:0,1:0};
+  q[1].rows.forEach( record => {
+    if (record['recommend'] === false) {
+      recommended[0] = record['count']
+    }
+    if (record['recommend'] === true) {
+      recommended[1] = record['count']
+    }
+  })
+  
+  let characteristics = {};
+  q[2].rows.forEach(type => {
+    characteristics[type['name']] = {
+      id : type['characteristic_id'],
+      value : type['value']
+    }
+  })
+
+
+  let meta = {
+    product_id : id,
+    ratings : ratings,
+    recommended : recommended,
+    characteristics : characteristics
+  }
+  return meta;
+}
 
 
 module.exports = {
@@ -41,9 +75,9 @@ module.exports = {
         `SELECT list_reviews.*, array_agg(review_photos.url) AS photos FROM list_reviews
        LEFT JOIN review_photos
        ON review_photos.review_id = list_reviews.id
-       WHERE list_reviews.product_id = ${req.params.product_id} AND list_reviews.reported = false
+       WHERE list_reviews.product_id = $1 AND list_reviews.reported = false
        GROUP BY list_reviews.id
-       LIMIT 50;`)
+       LIMIT 50;`,[req.params.product_id])
       .then(results => {res.send(listallFormat(results.rows));console.timeEnd()})
       .catch(err => console.log(err));
   },
@@ -53,24 +87,27 @@ module.exports = {
     pool
       .query(
         `SELECT rating, COUNT(rating) FROM list_reviews 
-       WHERE product_id = ${req.params.product_id} 
+       WHERE product_id = $1 
        GROUP BY rating;`
-      ),
+      ,[req.params.product_id]),
     pool.query(
-      `SELECT recommend, COUNT(recommend) FROM list_reviews WHERE product_id = ${req.params.product_id} AND recommend = true GROUP BY recommend`),
+      `SELECT recommend, COUNT(recommend) FROM list_reviews WHERE product_id = $1 GROUP BY recommend`,[req.params.product_id]),
     pool.query(
-      `Select characteristic_id, AVG(value) FROM characteristic_reviews 
-       WHERE characteristic_id IN (SELECT id from characteristics where product_id = ${req.params.product_id}) 
-       GROUP BY characteristic_id;`
+      `Select characteristics.name, characteristic_reviews.characteristic_id, AVG(characteristic_reviews.value) AS value 
+       FROM characteristic_reviews
+       LEFT JOIN characteristics
+       ON characteristics.id = characteristic_reviews.characteristic_id
+       WHERE characteristic_id IN (SELECT id from characteristics where product_id = $1) 
+       GROUP BY characteristic_reviews.characteristic_id, characteristics.name;`,[req.params.product_id]
     )
-    ]).then(results => {results.map(result => console.log(result.rows));console.timeEnd();res.sendStatus(200)})
+    ]).then(results => {res.send(metaFormat(results,req.params.product_id));console.timeEnd();})
   },
   helpful: (req, res) => {
     pool
       .query(
         `UPDATE list_reviews
        SET helpfulness = helpfulness + 1
-       WHERE id = ${req.params.review_id}`
+       WHERE id = $1`,[req.params.review_id]
       )
       .then(() => res.sendStatus(202))
       .catch(err => console.log(err));
@@ -80,7 +117,7 @@ module.exports = {
     .query(
       `UPDATE list_reviews
        SET reported = NOT reported
-       WHERE id = ${req.params.review_id}`)
+       WHERE id = $1`,[req.params.review_id])
     .then(()=> res.sendStatu(202))
     .catch(err => console.log(err))
   }
