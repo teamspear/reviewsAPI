0cd51aefdc88b206ed826a4bda795f784e085444
Tom Cernera <tomcernera@gmail.com>
2019-08-10 10:16:12 -0400

    updating loader io txt

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.get('/health', (req,res) => {
   res.sendStatus(200);
 })
 
-app.get('/loaderio-074622a3c066a1be3783ee43e2cb6362/', (req,res) => {
-  res.sendFile(__dirname+'/loader/loaderio-074622a3c066a1be3783ee43e2cb6362.txt');
+app.get('/loaderio-5b2f19396fa99d452da4172163af9461/', (req,res) => {
+  res.sendFile(__dirname+'/loader/loaderio-5b2f19396fa99d452da4172163af9461.txt');
 })
 
 app.get('/reviews/:product_id/list', (req,res) => {
